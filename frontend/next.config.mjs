e54bdb196bc394ebe8238101b4f9a2beec9b61ce
phamphamh <youssefb.prof@gmail.com>
2025-07-13 12:19:24 +0200

    pwa

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -35,4 +37,9 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
